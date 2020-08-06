c5275470951e0b1d46e8cb58c1ad6169401e6588
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-08-06 13:38:45 +0500

    Update ProductItem.js

--- src/Components/ProductItem.js
+++ src/Components/ProductItem.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import Shoes from '../shoes.json'
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
       padding: 20,
-      margin: 'auto',
     },
     img: {
         margin: 'auto',
         display: 'block',
         maxWidth: '100%',
         maxHeight: '100%',
-        marginBottom: 10
     },
-    grid: {
-        marginBottom: 50
+    detail: {
+        margin: 'auto',
+        display: 'block',
+        maxWidth: '100%',
+        maxHeight: '100%',
+
+
     }
 }));
 
@@ -40,10 +44,25 @@ function ProductItem() {
             <h1>Items</h1>
             <div className={classes.root}>
                 <Grid container spacing={3}>
-                    <Grid item xs={12} sm={6} lg={4} className={classes.grid}>                               
-                    <img className={classes.img} alt={shoe.name} src={shoe.img} />
-                        <Typography gutterBottom variant="h4" color="textPrimary">{shoe.name}</Typography>
+                    <Grid item xs={12} sm={6} >                               
+                        <img className={classes.img} alt={shoe.name} src={shoe.img} />
+                        
                     </Grid> 
+                    
+                    <Grid item xs={12} sm={6} className={classes.detail}>
+                        <Typography gutterBottom variant="h3" color="textPrimary">{shoe.name}</Typography>
+                        <Typography gutterBottom variant="body1" color="textSecondary" align='center' >{shoe.caption}</Typography>
+                        
+                        <Button variant="outlined" color="default">
+                               Add to Cart
+                           </Button> 
+                        
+                     </Grid>
+
+                     
+                    
+                    
+
                 </Grid>
             </div>
         </div>

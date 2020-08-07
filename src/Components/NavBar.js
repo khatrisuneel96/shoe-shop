4fc59196b8e0d079b831b483c228248955cfff36
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-08-07 23:58:16 +0500

    Responsive Navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,36 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import IconButton from '@material-ui/core/IconButton';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import CloseIcon from '@material-ui/icons/Close';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import { Link } from 'react-router-dom'
 
 function NavBar() {
-  return (
-    // <div className="navbar">
-    //   <Link to="/">Home</Link>
-    //   <Link to="/product" >Product</Link>
-    // </div>
 
+  const [toggle, setToggle] = useState(false)
+
+  // const menuToggle = () => {
+  //   setToggle(!toggle)
+  // }
+
+  return (
     <div>
       <header>
-        <div className="menu"></div>
+        <div className="menu" onClick={() => setToggle(!toggle)}>
+          <IconButton edge="start" color="inherit" aria-label="menu" >
+            <MenuIcon />
+          </IconButton>
+        </div>
         <div className="logo">
           <h1><Link to="/">NIKE</Link></h1>
         </div>
         
         <nav>
-          <ul>
+          <ul className={ toggle ? 'toggle': '' } onClick={()=>setToggle(false)}>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/aboutus">About Us</Link></li>
             <li><Link to="/products">Products</Link></li>
             <li><Link to="/contactus">Contact Us</Link></li>
+            <li className="close" onClick={() => setToggle(!toggle)}>
+              <CloseIcon />
+            </li>
           </ul>
 
           <div className="nav-cart">
             <span>0</span>
-            <ShoppingCartIcon />
+            <Link to="cart">
+              <ShoppingCartIcon color="action" />
+            </Link>
           </div>
-        </nav>
-          
-        
+        </nav>  
       </header>
     </div>
   )
--- src/RouteConfig.js
+++ src/RouteConfig.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from './Components/HomePage'
+import About from './Components/About'
 import Products from './Components/Products'
 import ProductItem from './Components/ProductItem'
+import Contact from './Components/Contact'
+import Cart from './Components/Cart'
 import NavBar from './Components/NavBar'
 import PageNotFound from './Components/PageNotFound'
 
@@ -14,8 +17,11 @@ function RouteConfig() {
                 <NavBar />
                 <Routes>
                     <Route path="/" element={<HomePage />} />
+                    <Route path="/aboutus" element={<About /> } />
                     <Route path="/products" element={<Products />} />
                     <Route path="/products/:id" element={<ProductItem />} />
+                    <Route path="/contactus" element={<Contact />} />
+                    <Route path="/cart" element={<Cart />} />
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </Router>
